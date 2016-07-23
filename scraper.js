93ebd023a7ab133c05c8d52d8d7aae52bae89e9f
Daniel <mwdchang@gmail.com>
2016-07-23 14:50:19 -0400

    update scraper

--- scraper.js
+++ scraper.js
@@ -7,7 +7,8 @@ var path = require('path');
 * Scrapes Morningstar HTML fragments for daily fund price changes
 */
 
-var base = 'http://quotes.morningstar.com/fund/c-header?t=';
+var base = 'http://quote.morningstar.com/fund/chart.aspx?t=';
+// var base = 'http://quotes.morningstar.com/fund/c-header?t=';
 var args = process.argv;
 var currency = '';
 
@@ -25,28 +26,50 @@ var len = 0; // For tracking last fund
 fs.readFile(file, 'utf-8', function(err, data) {
   config = JSON.parse(data);
   currency = config.currency || 'CAD';
-  len = config.funds.length;
-  config.funds.forEach(scrape)
+
+  len = config.portfolio.map(function(p) { return p.funds.length; }).reduce(function(a, b) { return a+b;});
+
+  config.portfolio.forEach(function(p) {
+    p.funds.forEach(function(f) {
+      scrape(f, p.name);
+    });
+  });
+
 });
 
+var result = [];
 
-function scrape(item, idx) {
+
+function scrape(item, port) {
   var url = base + item.symbol + '&cur=' + currency;
 
   request(url, function (error, statusCode, data) {
     var txt = normalize(data);
 
     /* Find change and change percentage */
+/*
     var change = txt.split('|')[0];
     var changePct = txt.split('|')[1];
+*/
+    var today = txt.split(',')[0].split(':')[1];
+    var yesterday = txt.split(',')[1].split(':')[1];
+
+    today = today.replace(/"/g, '');
+    yesterday = yesterday.replace(/"/g, '');
+
+
+    var change = (today - yesterday).toFixed(2);
+    var changePct = (100*change / yesterday).toFixed(2);
+
+
 
     /* If amount is specified, calculate gain/loss and overall gain/loss */
     if (item.amount) {
       var diff = (item.amount * +change);
       gainLoss += diff;
-      console.log(item.symbol, item.name, change, changePct, diff.toFixed(2));
+      console.log(port, item.symbol, item.name, change, changePct, diff.toFixed(2));
     } else {
-      console.log(item.symbol, item.name, change, changePct);
+      console.log(port, item.symbol, item.name, change, changePct);
     }
     len --;
 
@@ -60,10 +83,20 @@ function scrape(item, idx) {
 
 
 function normalize(data) {
+  var l = data.split('\n')[11];
+  var x = l.lastIndexOf('LastPrice');
+  var y = l.lastIndexOf('PercentChg');
+
+  return  l.substr(x, (y-x)-1);
+
   /* Scrape html fragment */
+/*
   var $ = cheerio.load(data);
+console.log('!!', data);
   var dayChange = $('[vkey="DayChange"]');
   var txt = dayChange.text();
+console.log('!!', txt);
   txt = txt.replace(/[\s\n]/g, '');
   return txt;
+*/
 }
